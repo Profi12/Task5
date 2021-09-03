4243d7fad72fc6171efc0a1af51837ca3fe030c2
Larisa Samoilenko <qwry28@gmail.com>
2021-09-03 17:44:12 +0300

    Added 5_5

--- Task5_Library/TaskHelper.cs
+++ Task5_Library/TaskHelper.cs
@@ -16,7 +16,7 @@
             return array[i, j];
         }
 
-       public static (int i, int j) MinIndex(int[,] array)
+        public static (int i, int j) MinIndex(int[,] array)
         {
             int minI = 0;
             int minJ = 0;
@@ -56,5 +56,54 @@
             return (maxI, maxJ);
         }
 
+        public static int GetNeighboursCounter(int[,] array)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    count += IsMoreThanNeighbours(array, i, j) ? 1 : 0;
+                }
+            }
+
+            return count;
+        }
+
+        private static bool IsMoreThanNeighbours(int[,] array, int i, int j)
+        {
+            bool moreThanNeighbours = true;
+            if ((i - 1) >= 0)
+            {
+                if (array[i, j] <= array[i - 1, j])
+                {
+                    moreThanNeighbours = false;
+                }
+            }
+            if ((i + 1) < array.GetLength(0))
+            {
+                if (array[i, j] <= array[i + 1, j])
+                {
+                    moreThanNeighbours = false;
+                }
+            }
+            if ((j - 1) >= 0)
+            {
+                if(array[i, j] <= array[i, j - 1])
+                {
+                    moreThanNeighbours = false;
+                }
+            }
+            if ((j + 1) < array.GetLength(1))
+            {
+                if (array[i, j] <= array[i, j + 1])
+                {
+                    moreThanNeighbours = false;
+                }
+            }
+
+            return moreThanNeighbours;
+        }
     }
 }
