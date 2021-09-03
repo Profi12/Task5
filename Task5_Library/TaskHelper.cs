d4cf49b870e5ed30d68def5f51a63b18938ad619
Larisa Samoilenko <qwry28@gmail.com>
2021-09-03 15:59:01 +0300

    Added 5_2

--- Task5_Library/TaskHelper.cs
+++ Task5_Library/TaskHelper.cs
@@ -21,5 +21,23 @@ namespace Task5_Library
 
             return min;
         }
+
+        public static int Max(int[,] array)
+        {
+            int max = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+
+            return max;
+        }
     }
 }
