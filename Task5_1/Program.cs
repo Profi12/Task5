317774c530e31a29314746329326e8109c819618
Larisa Samoilenko <qwry28@gmail.com>
2021-09-03 15:48:41 +0300

    Add project files.

---
+++ Task5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Task5_Library;
+
+namespace Task5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 4];
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(0, 100);
+                    Console.Write($"{array[i, j]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            
+            Console.WriteLine(TaskHelper.Min(array));
+        }
+    }
+}
---
+++ Task5_Library/TaskHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task5_Library
+{
+    public class TaskHelper
+    {
+        public static int Min(int[,] array)
+        {
+            int min = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (min > array[i, j])
+                    {
+                        min = array[i, j];
+                    }
+                }
+            }
+
+            return min;
+        }
+    }
+}
